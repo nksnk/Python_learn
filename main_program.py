0cf1af94bf7f27f18f0a834a81704acd8f523567
nksnk <nidhinksn@gmail.com>
2024-08-18 19:23:04 +0200

    changes added in main program

--- main_program.py
+++ main_program.py
@@ -5,3 +5,4 @@ login_module.login() #a function called login created to process login in main p
 print("successfully completed the login")
 output=multiply.multiplication(5,6)#multiply.py will run here
 print(output)
+print("Multiplication  completed")
