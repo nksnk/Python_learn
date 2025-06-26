5ec76e01fe7d35aa058d38b561e12ed899438462
nksnk <nidhinksn@gmail.com>
2025-06-26 19:45:11 +0200

    this for new test

---
+++ keywords_argument_function.py
@@ -0,0 +1,8 @@
+# def matchsummary(team,goal,penalty):
+#     print(team,goal,penalty)
+
+# matchsummary("brazil","5","6")
+def matchsummary(team,goal,penalty):
+    print(team,goal,penalty)
+
+matchsummary(goal=5,team="brazil",penalty=5)
